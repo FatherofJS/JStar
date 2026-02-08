c09217e7d6acd772d33846ef8b1cebdc14c7a9b0
nqtpx2207 <nqtpx2007@gmail.com>
2026-02-08 23:27:23 +0700

    'test'

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,13 @@
 // import { MOCK_CHART } from '../data/mockData';
 // import { MOCK_CHART } from '../data/mockData';
 
+import './Sidebar.css';
 export function Sidebar() {
-    return null;
+    return (
+    <nav>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam alias impedit quasi doloremque hic. Numquam quam eligendi a architecto, maxime autem veniam unde veritatis ratione error fuga, vero nobis possimus!
+    </nav>
+    )
+    
 }
+
