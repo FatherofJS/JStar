986a2f67929691dd3ff43b7fe53d7e5569d6ef59
anhtraan <quocanh08090002@gmail.com>
2026-02-26 08:25:08 +0700

    Position HousePanel and AspectPanel on right side of ChartWheel

--- src/components/ChartWheelWithTables.tsx
+++ src/components/ChartWheelWithTables.tsx
@@ -8,50 +8,37 @@ import type { ChartData } from '../types/chart';
 import { MOCK_CHART } from '../data/mockData';
 
 const Container = styled.div`
+    display: flex;
+    flex-direction: row;
     width: 100vw;
     min-height: 100vh;
     position: relative;
+`;
+
+const LeftSection = styled.div`
+    flex: 1;
     display: flex;
     justify-content: center;
     align-items: center;
-`;
-
-const ChartWrapper = styled.div`
-    position: relative;
-    width: 100%;
     height: 100vh;
+`;
+
+const RightSection = styled.div`
+    width: 400px;
     display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-// Table panels at corners
-const TopLeftPanel = styled.div`
-    position: absolute;
-    top: 20px;
-    left: 20px;
+    flex-direction: column;
+    gap: 16px;
+    padding: 20px;
+    padding-right: 30px;
+    max-height: 100vh;
+    overflow-y: auto;
+    background: rgba(10, 10, 20, 0.95);
+    position: relative;
     z-index: 100;
 `;
 
-const TopRightPanel = styled.div`
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    z-index: 100;
-`;
-
-const BottomLeftPanel = styled.div`
-    position: absolute;
-    bottom: 20px;
-    left: 20px;
-    z-index: 100;
-`;
-
-const BottomRightPanel = styled.div`
-    position: absolute;
-    bottom: 20px;
-    right: 20px;
-    z-index: 100;
+const TableWrapper = styled.div`
+    width: 100%;
 `;
 
 interface ChartWheelWithTablesProps {
@@ -69,7 +56,7 @@ export function ChartWheelWithTables(props: ChartWheelWithTablesProps) {
 
     return (
         <Container>
-            <ChartWrapper>
+            <LeftSection>
                 <ChartWheel
                     birthDate={birthDate}
                     birthTime={birthTime}
@@ -78,24 +65,16 @@ export function ChartWheelWithTables(props: ChartWheelWithTablesProps) {
                     timezone={timezone}
                     externalChartData={chartData}
                 />
-                
-                {/* 4 tables at 4 corners */}
-                <TopLeftPanel>
-                    <InfoPanel chartData={chartData} />
-                </TopLeftPanel>
-                
-                <TopRightPanel>
-                    <PlanetTable chartData={chartData} />
-                </TopRightPanel>
-                
-                <BottomLeftPanel>
-                    <AspectPanel chartData={chartData} />
-                </BottomLeftPanel>
-                
-                <BottomRightPanel>
+            </LeftSection>
+            
+            <RightSection>
+                <TableWrapper>
                     <HousePanel chartData={chartData} />
-                </BottomRightPanel>
-            </ChartWrapper>
+                </TableWrapper>
+                <TableWrapper>
+                    <AspectPanel chartData={chartData} />
+                </TableWrapper>
+            </RightSection>
         </Container>
     );
 }
